e57e729a388514d9d5af51fdabb2da0e0296de2a
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-02-02 13:04:56 +0200

    fixed dist and deploy to the render

--- dist/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isEmptyBody = void 0;
-var isEmptyBody_js_1 = require("./isEmptyBody.js");
-Object.defineProperty(exports, "isEmptyBody", { enumerable: true, get: function () { return __importDefault(isEmptyBody_js_1).default; } });
