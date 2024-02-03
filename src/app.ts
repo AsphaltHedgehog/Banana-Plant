15fbdaad46514064a730b913c48bbda0303bb13e
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-02-03 11:38:47 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,13 @@ const app = express();
 const formatsLogger: string =
     app.get('env') === 'development' ? 'dev' : 'short';
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
