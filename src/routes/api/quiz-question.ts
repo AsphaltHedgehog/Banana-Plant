0aba694f45c2235c9e049ccf1a34f2253e426138
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-02-04 02:05:59 +0200

    complete route

---
+++ src/routes/api/quiz-question.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+import quizQuestion from "../../controllers/quiz-question.js";
+
+import { isEmptyBody } from "../../middlewares/index.js";
+
+// Пока не добавляю аутефикацию чтобы фронту было удобнее работать
+// import { authenticate } from '../../middlewares';
+
+const quizQuestionRoute = express.Router();
+
+quizQuestionRoute.post("/", isEmptyBody, //authenticate,
+  quizQuestion.addNewQuizQuestion);
+
+quizQuestionRoute.patch('/:id', isEmptyBody, //authenticate,
+  quizQuestion.updateQuizQuestionById);
+
+quizQuestionRoute.delete('/:id', //authenticate,
+  quizQuestion.deleteQuizQuestionById);
+
+// quizQuestionRoute.delete('/img/:id', //authenticate,
+//   quizQuestion.deleteQuizQuestionImgById);
+
+export default quizQuestionRoute;
